35d74af00901f70a4a8c2a13db506171d4949eb4
Ludovic Fournier <fouludovic@gmail.com>
2025-10-14 22:59:49 -0400

    Adding train detail in station menu

--- src/api.js
+++ src/api.js
@@ -1,6 +1,12 @@
 async function refreshStatuses() {
   try {
-    const res = await fetch(`${CONFIG.API_URL}/api/exo/trains/stations`, { cache: "no-store" });
+    const res = await fetch(`${CONFIG.API_URL}/api/exo/trains/stations`, {
+      method: "GET",
+      headers: {
+        cache: "no-store",
+        train_info: "true",
+      },
+    });
     if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
     const data = await res.json();
     updateDots(data);
@@ -12,7 +18,9 @@ async function refreshStatuses() {
 
 async function refreshStatusesR() {
   try {
-    const res = await fetch(`${CONFIG.API_URL}/api/exo/trains/rails`, { cache: "no-store" });
+    const res = await fetch(`${CONFIG.API_URL}/api/exo/trains/rails`, {
+      cache: "no-store",
+    });
     if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
     const data = await res.json();
     updateDotsR(data);
@@ -23,17 +31,45 @@ async function refreshStatusesR() {
 }
 
 function updateDots(items) {
-  
-  for (const { id, status } of items) {
-    const dot = dotsById.get(id);
+  for (const station of items) {
+    const dot = dotsById.get(station.id);
     if (!dot) continue;
     dot.classList.remove(...Object.values(STATUS_CLASS));
-    dot.classList.add(STATUS_CLASS[status] || STATUS_CLASS.offline);
+    dot.classList.add(STATUS_CLASS[station.status] || STATUS_CLASS.offline);
+
+    // Update status box inside menu
+    const menuBody = dot.querySelector(".dot-menu-body");
+    const statusBox = dot.querySelector(".status-box");
+    const wagonDetailTable = document.createElement("table");
+
+    menuBody.innerHTML = ``;
+
+    if (station.train) {
+      menuBody.innerHTML = `<h3>Détail du train : </h3>`;
+      wagonDetailTable.innerHTML = `
+      <h4>Occupation : ${station.occupancyStatus} </h4>
+      <h4>Composition : </h4> <tr>
+      <th>Ordre</th>
+      <th>Id</th> </tr>`;
+
+      for (const wagon of station.train) {
+        const wagonDetail = document.createElement("tr");
+        wagonDetail.innerHTML = `<td> ${wagon.carriageSequence}</td> <td>  ${wagon.id} </td>`;
+        wagonDetailTable.appendChild(wagonDetail);
+      }
+      menuBody.appendChild(wagonDetailTable);
+    }
+
+    if (statusBox) {
+      statusBox.classList.remove(...Object.values(STATUS_CLASS));
+      statusBox.classList.add(
+        STATUS_CLASS[station.status] || STATUS_CLASS.offline
+      );
+    }
   }
 }
 
 function updateDotsR(items) {
- 
   for (const { id, status } of items) {
     const dot = dotsById.get(id);
     if (!dot) continue;
