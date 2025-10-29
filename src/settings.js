e35a93530410dbea100f7bd78d4b602654e2f81d
Ludovic Fournier <fouludovic@gmail.com>
2025-10-29 19:09:54 -0400

    Adding a setting button to only toggle rail segement dots also fixing typo mistake

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 //settings.js
 function initSettings() {
-  let saved = (() => {
+  let saved_fan_set = (() => {
     try {
       return localStorage.getItem("rail-fan-mode");
     } catch {
@@ -8,12 +8,24 @@ function initSettings() {
     }
   })();
 
-  saved = (saved === "true")
+  let saved_segment_set = (() => {
+    try {
+      return localStorage.getItem("rail-segment-mode");
+    } catch {
+      return null;
+    }
+  })();
 
-  const check = document.getElementById("RailFanSwitch");
-  check.checked = saved;
+  saved_fan_set = saved_fan_set === "true";
+  saved_segment_set = saved_segment_set === "true";
 
-  setSettings(saved || 0);
+  const check_fan = document.getElementById("RailFanSwitch");
+  check_fan.checked = saved_fan_set;
+
+  const check_segment = document.getElementById("SegmentSwitch");
+  check_segment.checked = saved_segment_set;
+
+  setSettings(saved_fan_set || 0, saved_segment_set || 0);
   document.querySelectorAll(".setting-btn").forEach((btn) => {
     btn.addEventListener("click", () => openSettingsMenu());
   });
@@ -27,13 +39,14 @@ function closeSettingsMenu() {
   document.getElementById("settings-overlay").style.display = "none";
 }
 
-function changeRailFanMode() {
-  const check = document.getElementById("RailFanSwitch");
+function changeSetting() {
+  const check_fan = document.getElementById("RailFanSwitch");
+  const check_segment = document.getElementById("SegmentSwitch");
 
-  setSettings(check.checked);
+  setSettings(check_fan.checked, check_segment.checked);
 }
 
-function setSettings(railFanMode) {
+function setSettings(railFanMode, railSegmentmode) {
   if (railFanMode) {
     document.querySelectorAll(".rail-fan-element-table").forEach((el) => {
       el.style.display = "table";
@@ -50,8 +63,22 @@ function setSettings(railFanMode) {
      });
   }
 
+  if (railSegmentmode) {
+    document.querySelectorAll(".rail-fan-element-segment").forEach((el) => {
+      el.style.display = "flex";
+    });
+  } else {
+    document.querySelectorAll(".rail-fan-element-segment").forEach((el) => {
+      el.style.display = "none";
+    });
+  }
+
   
   try {
     localStorage.setItem("rail-fan-mode", railFanMode);
   } catch {}
+
+   try {
+     localStorage.setItem("rail-segment-mode", railSegmentmode);
+   } catch {}
 }
