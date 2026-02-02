89c0884854a1ec1812dfc460b207049c1fb6091e
Ludovic Fournier <fouludovic@gmail.com>
2026-02-02 15:14:34 -0500

    Fixing css cache issue

--- main.js
+++ main.js
@@ -4,25 +4,25 @@ async function loadUI() {
   if (isMobile) {
     document.head.insertAdjacentHTML(
       "beforeend",
-      `<link rel="stylesheet" href="./mobile/main.css">
-       <link rel="stylesheet" href="./mobile/style/menu.css">
-       <link rel="stylesheet" href="./mobile/style/navbar.css">
-       <link rel="stylesheet" href="./mobile/style/settings.css">`
+      `<link rel="stylesheet" href="./mobile/main.css?v=4">
+       <link rel="stylesheet" href="./mobile/style/menu.css?v=4">
+       <link rel="stylesheet" href="./mobile/style/navbar.css?v=4">
+       <link rel="stylesheet" href="./mobile/style/settings.css?v=4">`
     );
     const mod = await import("./mobile/app.js?v=4");
     mod.init(); 
   } else {
     document.head.insertAdjacentHTML(
       "beforeend",
-      `<link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css"/>
-       <link rel="stylesheet" href="./desktop/main.css">
-       <link rel="stylesheet" href="./desktop/style/menu.css">
-       <link rel="stylesheet" href="./desktop/style/navbar.css">
-       <link rel="stylesheet" href="./desktop/style/settings.css">
-       <link rel="stylesheet" href="./desktop/style/consists.css">
-       <link rel="stylesheet" href="./desktop/style/realMap/realMap.css">
-       <link rel="stylesheet" href="./desktop/style/realMap/pin.css">
-       <link rel="stylesheet" href="./desktop/style/realMap/panel.css">`
+      `<link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css?v=4"/>
+       <link rel="stylesheet" href="./desktop/main.css?v=4">
+       <link rel="stylesheet" href="./desktop/style/menu.css?v=4">
+       <link rel="stylesheet" href="./desktop/style/navbar.css?v=4">
+       <link rel="stylesheet" href="./desktop/style/settings.css?v=4">
+       <link rel="stylesheet" href="./desktop/style/consists.css?v=4">
+       <link rel="stylesheet" href="./desktop/style/realMap/realMap.css?v=4">
+       <link rel="stylesheet" href="./desktop/style/realMap/pin.css?v=4">
+       <link rel="stylesheet" href="./desktop/style/realMap/panel.css?v=4">`
     );
     const app = await import("./desktop/app.js?v=4");
    
